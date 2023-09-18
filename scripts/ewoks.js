da12bc8b3f4cdf01fcb86dd3a927fe0a2f9e9577
poshak-rialtes <138452224+poshak-rialtes@users.noreply.github.com>
2023-09-18 12:43:24 +0530

    Added step us logic for performance

--- scripts/ewoks.js
+++ scripts/ewoks.js
@@ -8,7 +8,7 @@ const default_vus = 10000;
 const target_vus_env = `${__ENV.TARGET_VUS}`;
 const target_vus = isNumeric(target_vus_env) ? Number(target_vus_env) : default_vus;
 
-export let options = {
+export let options1 = {
   stages: [
       // Ramp-up from 1 to TARGET_VUS virtual users (VUs) in 5s
       { duration: "1s", target: target_vus },
@@ -26,6 +26,49 @@ export let options = {
 //   check(response, { "status is 200": (r) => r.status === 200 });
 //   sleep(.300);
 // };
+
+export const options = {
+
+  scenarios: {
+    basic: {
+      executor: 'ramping-vus',
+      startTime: '0s',
+      gracefulStop: '1s',
+      gracefulRampDown: '0s',
+      stages: [
+        { duration: '10s', target: 40 },
+      ]
+    },
+    high_load: {
+      executor: 'ramping-vus',
+      startTime: '11s',
+      gracefulStop: '1s',
+      gracefulRampDown: '0s',
+      stages: [
+        { duration: '10s', target: 200 },
+      ]
+    },
+    spike: {
+      executor: 'ramping-vus',
+      startTime: '22s',
+      gracefulStop: '1s',
+      gracefulRampDown: '0s',
+      stages: [
+        { duration: '10s', target: 5000 },
+      ]
+    }
+  },
+
+  thresholds: {
+    http_req_failed: ['rate < 0.01'],
+    http_req_duration: ['p(90) < 100', 'p(95) < 200', 'p(99) < 500'],
+  },
+
+  noConnectionReuse: true,
+
+  userAgent: 'MyK6UserAgentString/1.0',
+
+};
 
 
 export default function () {
