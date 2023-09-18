4c58378012191ffae7b0de795557091e6dea76fe
poshak-rialtes <138452224+poshak-rialtes@users.noreply.github.com>
2023-09-18 12:21:17 +0530

    Load Testing for applications

--- scripts/ewoks.js
+++ scripts/ewoks.js
@@ -3,7 +3,7 @@ import { check, sleep } from "k6";
 
 const isNumeric = (value) => /^\d+$/.test(value);
 
-const default_vus = 5;
+const default_vus = 1000;
 
 const target_vus_env = `${__ENV.TARGET_VUS}`;
 const target_vus = isNumeric(target_vus_env) ? Number(target_vus_env) : default_vus;
@@ -11,18 +11,25 @@ const target_vus = isNumeric(target_vus_env) ? Number(target_vus_env) : default_
 export let options = {
   stages: [
       // Ramp-up from 1 to TARGET_VUS virtual users (VUs) in 5s
-      { duration: "5s", target: target_vus },
+      { duration: "1s", target: target_vus },
 
       // Stay at rest on TARGET_VUS VUs for 10s
-      { duration: "10s", target: target_vus },
+      { duration: "1s", target: target_vus },
 
       // Ramp-down from TARGET_VUS to 0 VUs for 5s
-      { duration: "5s", target: 0 }
+      { duration: "1s", target: 0 }
   ]
 };
 
+// export default function () {
+//   const response = http.get("https://localhost:8082", {headers: {Accepts: "application/json"}});
+//   check(response, { "status is 200": (r) => r.status === 200 });
+//   sleep(.300);
+// };
+
+
 export default function () {
-  const response = http.get("https://swapi.dev/api/people/30/", {headers: {Accepts: "application/json"}});
+  const response = http.get("http://docker.for.mac.host.internal:8082/webhook/");
   check(response, { "status is 200": (r) => r.status === 200 });
   sleep(.300);
 };
